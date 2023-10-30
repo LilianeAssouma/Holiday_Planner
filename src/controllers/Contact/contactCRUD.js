7014291a9867e97cb784e396c834ca5fceb4d729
LilianeAssouma <lilyanassoum@gmail.com>
2023-10-30 08:42:29 +0200

    v5.7  commit

--- src/controllers/Contact/contactCRUD.js
+++ src/controllers/Contact/contactCRUD.js
@@ -10,71 +10,71 @@ import { transporter } from "../../utils/Creditentials.js";
 
 
 
-export const submitForm = async (req, res) => { 
-  try {
-   
-    const { email, message } = req.body;
-    // const newContact = new Contact({
-    //   email,
-    //   message
-    // });
-    res.status(201).json({ message: 'Contact created successfully',
-     contact:{email, message}
-     });
-  } catch (error) {
-  
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-}
-
-
-
-// export const submitForm = async (req, res) => {                                     
-
+// export const submitForm = async (req, res) => { 
 //   try {
-
-//     // if (!email) {
-//     //   return res.status(400).json({ error: 'Email is required' });
-//     // }
-
-//     // const existingEmail = await Contact.findOne({ email });
-
-//     // if (!existingEmail) {
-//     //   return res.status(401).json({ error: 'Email not found' });
-//     // }
-//     const {  message } = req.body;
-//     const email = req.body.email; 
-
-//     console.log(' email:', email);
-
-//     const newContact = await Contact.create({  email, message });
-
-
-//    console.log(email);
-
-//     const mailOptions = {
-//       to: newContact.email,
-//       from: "lilyanassoum@gmail.com" ,
-//       subject: 'Contact Form Submission',
-//       text: `Email: ${email}\nMessage: ${message}`,
-//     };
-
-//     transporter.sendMail(mailOptions, (error, info) => {
-//       if (error) {
-//         console.error('Email error:', error);
-//       } else {
-//         console.log('Email sent:', info.response);
-//       }
+   
+//     const { email, message } = req.body;
+//     const newContact = new Contact({
+//       email,
+//       message
 //     });
-
-//     res.status(200).json({ message: 'Form submitted successfully!' });
+//     res.status(201).json({ message: 'Contact created successfully',
+//      newContact
+//      });
 //   } catch (error) {
-
-//     console.error('Form submission error:', error);
+  
+//     console.error(error);
 //     res.status(500).json({ message: 'Internal server error' });
 //   }
-// };
+// }
+
+
+
+export const submitForm = async (req, res) => {                                     
+
+  try {
+
+    // if (!email) {
+    //   return res.status(400).json({ error: 'Email is required' });
+    // }
+
+    // const existingEmail = await Contact.findOne({ email });
+
+    // if (!existingEmail) {
+    //   return res.status(401).json({ error: 'Email not found' });
+    // }
+    const {  message } = req.body;
+    const email = req.body.email; 
+
+    console.log(' email:', email);
+
+    const newContact = await Contact.create({  email, message });
+
+
+   console.log(email);
+
+    // const mailOptions = {
+    //   to: newContact.email,
+    //   from: "lilyanassoum@gmail.com" ,
+    //   subject: 'Contact Form Submission',
+    //   text: `Email: ${email}\nMessage: ${message}`,
+    // };
+
+    // transporter.sendMail(mailOptions, (error, info) => {
+    //   if (error) {
+    //     console.error('Email error:', error);
+    //   } else {
+    //     console.log('Email sent:', info.response);
+    //   }
+    // });
+
+    res.status(200).json({ message: 'Form submitted successfully!' });
+  } catch (error) {
+
+    console.error('Form submission error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
 
 
